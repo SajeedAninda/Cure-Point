61568108bd5967ee3b41455eeb11655ce18f9b04
SajeedAninda <sajaninda@gmail.com>
2025-01-14 17:13:48 +0600

    navbar bg change on scroll

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../../src/assets/logo.png'
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = window.innerHeight * 0.17
+      if (window.scrollY > scrollThreshold) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <div className='h-[14vh] bg-[#07332F] text-white flex items-center bg-transparent z-50 fixed top-0 left-0 w-full'>
+    <div
+      className={`h-[13vh] text-white flex items-center z-50 fixed top-0 left-0 w-full transition-all duration-300 ${
+        scrolled ? 'bg-[#07332F]' : 'bg-transparent'
+      }`}
+    >
       <div className='w-[95%] lg:w-[1150px] mx-auto flex justify-between items-center'>
         <div>
           <img className='w-[150px]' src={logo} alt='' />
