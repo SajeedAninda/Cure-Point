ce02dedeb894b7c351abd987d3b0f61ea8a5fec8
SajeedAninda <sajaninda@gmail.com>
2025-01-14 16:36:12 +0600

    Completed Services section

--- src/Homepage/Services/Services.jsx
+++ src/Homepage/Services/Services.jsx
@@ -19,12 +19,12 @@ const Services = () => {
 
   return (
     <div className='h-fit'>
-      <div className='w-[90%] lg:w-[1150px] mx-auto flex justify-between items-center gap-6 py-20'>
+      <div className='w-[90%] lg:w-[1150px] mx-auto flex justify-between items-stretch gap-12 py-28'>
         <div className='imgDiv flex-1'>
           <img src={doctorImg} alt='' />
         </div>
 
-        <div className='textDiv flex-1'>
+        <div className='textDiv flex-1 flex flex-col justify-between'>
           <h1 className='text-[#1E1E1E] text-[40px] font-bold'>Our Services</h1>
           <p className='text-[16px] text-gray-500 font-normal mt-3'>
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -48,12 +48,33 @@ const Services = () => {
               </button>
             ))}
           </div>
-          <div className='tabImages mt-4'>
+          <div className='tabImages rounded-xl mt-4'>
             <img
               className='rounded-xl object-contain w-full h-[350px]'
               src={tabImages[activeTab]}
               alt={activeTab}
             />
+          </div>
+
+          <div className='bottomDiv mt-4'>
+            <h1 className='text-[#1E1E1E] text-[40px] font-bold'>
+              Electro Gastrology Therapy
+            </h1>
+            <p className='text-[16px] text-gray-500 font-normal mt-3'>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inve ntore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus
+              error
+            </p>
+            <p className='text-[16px] text-gray-500 font-normal mt-3'>
+              Sit voluptatem accusantium doloremque laudantium, totam rem
+              aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi
+              architecto beatae vitae dicta sunt explicabo.
+            </p>
+            <button className='text-[#F7A582] text-[16px] font-bold bg-transparent border-2 border-[#F7A582] px-6 py-3 transition-all duration-300 ease-in-out rounded-xl mt-8 hover:bg-[#F7A582] hover:text-white'>
+              More Details
+            </button>
           </div>
         </div>
       </div>
