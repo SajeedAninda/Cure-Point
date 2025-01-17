63124dacbd8522b7cd18b79980c5ff8258ed5815
SajeedAninda <sajaninda@gmail.com>
2025-01-17 18:02:20 +0600

    Added bg image in appointment header

--- src/Appointment/Appointment.jsx
+++ src/Appointment/Appointment.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from 'react'
+import pillImg from '../assets/bg-pill2.png'
+import vectorImg from '../assets/Vector.png'
 
 const Appointment = () => {
-    return (
-        <div>
-            
+  return (
+    <div className=''>
+      <div className='headerDiv bg-[#07332F] h-[50vh] w-full '>
+        <div className='w-[95%] lg:w-[1150px] h-full mx-auto flex flex-col justify-center items-start relative'>
+          <img
+            className='absolute bottom-0 left-0 rotate-180 w-[200px]'
+            src={vectorImg}
+            alt=''
+          />
+          <p className='text-white  text-[16px]'>Home / Appointment</p>
+          <p className='text-white  text-[30px] font-bold mt-3'>Appointment</p>
+          <img className='absolute -right-20 w-[200px] top-16' src={pillImg} alt="" />
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default Appointment;
+export default Appointment
