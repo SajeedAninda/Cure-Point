b3763cdb52cec3e158269ea7e02d04caa3e283e3
SajeedAninda <sajaninda@gmail.com>
2025-01-21 17:31:32 +0600

    Functionality to update payment status

--- src/UserDashboard/UserAppointments/UserAppointments.jsx
+++ src/UserDashboard/UserAppointments/UserAppointments.jsx
@@ -33,11 +33,42 @@ const UserAppointments = () => {
     })
   }
 
-  if (loading) {
-    return <div className='text-left text-6xl text-[#191919] font-bold'>Loading...</div>
+  let handlePay = id => {
+    Swal.fire({
+      title: 'Do you want to Pay for this Appointment?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Pay'
+    }).then(result => {
+      if (result.isConfirmed) {
+        axiosInstance.patch(`/appointmentStatus/${id}`).then(res => {
+          if (res.data.modifiedCount >= 1) {
+            refetch()
+            Swal.fire({
+              title: 'Paid!',
+              text: 'You Have Paid for Your Appointment.',
+              icon: 'success'
+            })
+          }
+        })
+      }
+    })
   }
 
-  let unpaidAppointments = appointmentData?.filter(data => data.paymentStatus === 'unpaid')
+  if (loading) {
+    return (
+      <div className='text-left text-6xl text-[#191919] font-bold'>
+        Loading...
+      </div>
+    )
+  }
+
+  let unpaidAppointments = appointmentData?.filter(
+    data => data.paymentStatus === 'unpaid'
+  )
 
   return (
     <div className='w-[75%] text-[22px] px-10 py-6'>
@@ -45,7 +76,9 @@ const UserAppointments = () => {
         Scheduled Unpaid Appointments: {unpaidAppointments?.length || 0}
       </h3>
       {unpaidAppointments?.length === 0 ? (
-        <div className='text-center text-lg font-semibold text-gray-500'>No data found</div>
+        <div className='text-center text-lg font-semibold text-gray-500'>
+          No data found
+        </div>
       ) : (
         <div className='table w-full mt-4'>
           <div className='w-full py-3 grid grid-cols-12 items-center bg-[#E6E6E6] rounded-t-xl text-[#191919] text-[14px] font-bold'>
@@ -59,14 +92,20 @@ const UserAppointments = () => {
           </div>
 
           {unpaidAppointments?.map((data, index) => (
-            <div key={data._id} className='w-full py-3 grid grid-cols-12 items-center bg-white text-[#898989] text-[14px] font-semibold'>
+            <div
+              key={data._id}
+              className='w-full py-3 grid grid-cols-12 items-center bg-white text-[#898989] text-[14px] font-semibold'
+            >
               <div className='col-span-1 flex justify-center'>{index + 1}</div>
               <div className='col-span-3'>{data.name}</div>
               <div className='col-span-2'>{data.selectedDate}</div>
               <div className='col-span-2'>{data.selectedTime}</div>
               <div className='col-span-2'>{data.selectedService?.name}</div>
               <div className='col-span-1'>
-                <button className='px-4 py-3 bg-[#07332F] text-white rounded-lg hover:bg-opacity-50 transition-all duration-150'>
+                <button
+                  onClick={() => handlePay(data._id)}
+                  className='px-4 py-3 bg-[#07332F] text-white rounded-lg hover:bg-opacity-50 transition-all duration-150'
+                >
                   Pay
                 </button>
               </div>
