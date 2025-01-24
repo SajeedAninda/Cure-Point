d19ff287cbf7409f28336bbe62245ffecb186f24
SajeedAninda <sajaninda@gmail.com>
2025-01-24 15:40:49 +0600

    Image uploading feature added through imgBB

--- src/AdminDashboard/AddDoctors/AddDoctors.jsx
+++ src/AdminDashboard/AddDoctors/AddDoctors.jsx
@@ -1,27 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaUpload } from 'react-icons/fa'
+import useAxiosInstance from '../../Hooks/useAxiosInstance'
+import Swal from 'sweetalert2'
 
 const AddDoctors = () => {
+  const [doctorName, setDoctorName] = useState('')
+  const [doctorEmail, setDoctorEmail] = useState('')
+  const [speciality, setSpeciality] = useState('')
+  const [imageFile, setImageFile] = useState(null)
+  const [imageUrl, setImageUrl] = useState('')
+
+  const imgBBAPI = 'cbd289d81c381c05afbab416f87e8637'
+  let axiosInstance = useAxiosInstance()
+
+  const handleFileChange = event => {
+    setImageFile(event.target.files[0])
+  }
+
+  const handleFormSubmit = async event => {
+    event.preventDefault()
+
+    if (!imageFile || !doctorName || !doctorEmail || !speciality) {
+      Swal.error({
+        icon: 'error',
+        title: 'Oops',
+        text: 'Please submit all fields correctly'
+      })
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('image', imageFile)
+
+    const response = await axiosInstance.post(
+      `https://api.imgbb.com/1/upload?key=${imgBBAPI}`,
+      formData
+    )
+
+    if (response.data.success) {
+      const uploadedImageUrl = response.data.data.display_url
+      setImageUrl(uploadedImageUrl)
+
+      const doctorDetails = {
+        name: doctorName,
+        email: doctorEmail,
+        speciality: speciality,
+        image: uploadedImageUrl
+      }
+
+    }
+  }
+
   return (
     <div className='w-[75%] h-fit px-10 py-6'>
       <h3 className='text-[24px] font-bold text-[#191919]'>Add A New Doctor</h3>
-      <form className='bg-white w-[60%] rounded-lg h-fit px-10 py-12 mt-6'>
+      <form
+        onSubmit={handleFormSubmit}
+        className='bg-white w-[60%] rounded-lg h-fit px-10 py-12 mt-6'
+      >
         <div>
-          <label htmlFor='doctorName' className=' font-semibold text-[#191919]'>
+          <label htmlFor='doctorName' className='font-semibold text-[#191919]'>
             Name
           </label>
           <br />
           <input
             className='py-3 mt-2 rounded-lg px-4 placeholder:text-gray-400 border-2 border-gray-300 w-full'
             type='text'
-            placeholder='Please Enter Doctors Name'
+            placeholder='Please Enter Doctor’s Name'
+            value={doctorName}
+            onChange={e => setDoctorName(e.target.value)}
           />
         </div>
 
         <div className='mt-6'>
           <label
             htmlFor='doctorsEmail'
-            className=' font-semibold text-[#191919]'
+            className='font-semibold text-[#191919]'
           >
             Email
           </label>
@@ -29,23 +83,23 @@ const AddDoctors = () => {
           <input
             className='py-3 mt-2 rounded-lg px-4 placeholder:text-gray-400 border-2 border-gray-300 w-full'
             type='email'
-            placeholder='Please Enter Doctors Email'
+            placeholder='Please Enter Doctor’s Email'
+            value={doctorEmail}
+            onChange={e => setDoctorEmail(e.target.value)}
           />
         </div>
 
         <div className='mt-6'>
-          <label
-            htmlFor='doctorsEmail'
-            className=' font-semibold text-[#191919]'
-          >
+          <label htmlFor='speciality' className='font-semibold text-[#191919]'>
             Speciality
           </label>
           <br />
           <select
-            className='py-3 mt-2 rounded-lg px-4 placeholder:text-gray-400 border-2 border-gray-300 w-full'
-            name=''
-            id=''
+            className='py-3 mt-2 rounded-lg px-4 border-2 border-gray-300 w-full'
+            value={speciality}
+            onChange={e => setSpeciality(e.target.value)}
           >
+            <option value=''>Select Speciality</option>
             <option value='orthodontics'>Teeth Orthodontics</option>
             <option value='cosmetic'>Cosmetic Dentistry</option>
             <option value='teethCleaning'>Teeth Cleaning</option>
@@ -55,12 +109,32 @@ const AddDoctors = () => {
           </select>
         </div>
 
-        <div className='mt-10 w-full border-2 border-dotted border-gray-400 p-10 rounded-lg flex flex-col justify-center items-center gap-3 cursor-pointer hover:bg-gray-200 transition-all duration-200'>
-          <p className='text-gray-500 font-semibold'>Upload Your Photo</p>
-          <FaUpload className='text-gray-500'/>
+        <input
+          type='file'
+          id='fileInput'
+          className='hidden'
+          onChange={handleFileChange}
+          accept='image/*'
+        />
+        <div
+          className='mt-10 w-full border-2 border-dotted border-gray-400 p-10 rounded-lg flex flex-col justify-center items-center gap-3 cursor-pointer hover:bg-gray-200 transition-all duration-200'
+          onClick={() => document.getElementById('fileInput').click()}
+        >
+          {imageFile ? (
+            <p className='text-gray-500 font-semibold'>{imageFile.name}</p>
+          ) : (
+            <>
+              <p className='text-gray-500 font-semibold'>Upload Your Photo</p>
+              <FaUpload className='text-gray-500' />
+            </>
+          )}
         </div>
-        <button className='w-full py-3 rounded-lg bg-[#07332F] text-white hover:bg-opacity-50 transition-all duration-300 mt-6'>
-            Add
+
+        <button
+          type='submit'
+          className='w-full py-3 rounded-lg bg-[#07332F] text-white hover:bg-opacity-50 transition-all duration-300 mt-6'
+        >
+          Add
         </button>
       </form>
     </div>
